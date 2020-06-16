68a85c7e60ea01dedda9e9ce3bae39dc76dc845a
Carl-Be <carlbechie@carlbechie.com>
2020-06-16 05:44:24 -0400

    How I like it for now

--- enum.py
+++ enum.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 
 import subprocess as sp
+import os
 
 target = raw_input("Enter Target: ")
 export = "export TARGET=" + target
-sp.call(export, shell=True)
 
-sp.call("xdotool key \"ctrl+shift+t\"; xdotool type \" nmap -T4 -oX nmap.xml -vv -p 80 10.0.2.15 && xsltproc nmap.xml -o nmap.html && firefox nmap.html\"; xdotool key Return", shell=True)
+os.putenv("TARGET", target)
 
 sp.call("xdotool key \"ctrl+shift+t\"; xdotool type \" gobuster dir -t 90 -w /usr/share/dirb/wordlists/common.txt -u $TARGET\"; xdotool key Return", shell=True)
 
-"""
-sp.call("xdotool key \"ctrl+shift+t\"; xdotool type \" nmap -T4 -oX nmap.xml -vv -p 80 10.0.2.15 && xsltproc nmap.xml -o nmap.html && firefox nmap.html\"; xdotool key Return", shell=True)
+sp.call("xdotool key \"ctrl+shift+t\"; xdotool type \" gobuster dir -t 90 -w /usr/share/dirbuster/wordlists/directory-list-2.3-medium.txt -u $TARGET\"; xdotool key Return", shell=True)
 
-sp.call("xdotool key \"ctrl+shift+t\"; xdotool type \" nmap -T4 -oX nmap.xml -vv -p 80 10.0.2.15 && xsltproc nmap.xml -o nmap.html && firefox nmap.html\"; xdotool key Return", shell=True)
-"""
+sp.call("xdotool key \"ctrl+shift+t\"; xdotool type \" nikto -h $TARGET\"; xdotool key Return", shell=True)
+
+sp.call("xdotool key \"ctrl+shift+t\"; xdotool type \" sudo nmap -A -T4 -oX nmap.xml -vv $TARGET && xsltproc nmap.xml -o nmap.html\"; xdotool key Return", shell=True)
